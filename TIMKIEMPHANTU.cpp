ce0114cff7f76923c1a0cf03b7dbddde1ca06638
Nguyễn Chính <115388632+chinhnguyen34@users.noreply.github.com>
2024-02-10 23:46:34 +0700

    Update TIMKIEMPHANTU.cpp

--- TIMKIEMPHANTU.cpp
+++ TIMKIEMPHANTU.cpp
@@ -1,2 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int cnt[1000001];
 
+int main () {
+
+  int n; cin >> n;
+  int a[n];
+
+  for(int& x : a) cin >> x;
+  for(int i = 0; i < n; i ++) {
+    cnt[ a[i] ] = 1;
+  }
+  for(int i = 0; i <= 1000001; i ++){
+    if(cnt[i] == 1) {
+      
+    }
+  }
+  
+}
